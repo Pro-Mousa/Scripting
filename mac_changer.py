352dba58b89a7172ff77518781a4023101192ca5
Mousa Muigai <mousa@mousa.Mousa>
2026-04-29 22:17:34 +0300

    MAC Changer

--- mac_changer.py
+++ mac_changer.py
@@ -1,5 +1,6 @@
 import subprocess
 import optparse
+import re
 
 ## Get User Input
 def get_user_input():
@@ -17,15 +18,25 @@ def mac_address_changer(user_interface, user_mac_address):
 
 ## Checking if MAC_Address has been changed
 def control_new_mac(user_interface):
-    ifconfig = subprocess.check_output(["ifconfig",user_interface])
-    print(ifconfig)
+    ifconfig = subprocess.check_output(["ifconfig",user_interface]).decode()
+    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",ifconfig)
 
+    if new_mac:
+        return new_mac.group(0)
+    else:
+        return None
 
 print("MAC_Changer Started !!")
+
 ## Calling get_user_input function and passing the tuples to mac_address_changer function
 (user_input,arguments) = get_user_input()
 mac_address_changer(user_input.wifi_interface,user_input.mac_address)
-control_new_mac(user_input.wifi_interface)
+final_mac = control_new_mac(user_input.wifi_interface)
+
+if final_mac == user_input.mac_address:
+    print("MAC Address changed successfully!!")
+else:
+    print("MAC Address Failed. Try Again!!")
 
 
 print("MAC_Changer successfully Executed !!")
